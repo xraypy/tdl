34440da67dcfb182f478833166693898fd00adcc
newville <newville@cc796bef-3d10-0410-8362-fc8e0e1515e5>
2006-04-11 22:29:08 +0000

    git-svn-id: http://millenia.cars.aps.anl.gov/svn/tdl/trunk@41 cc796bef-3d10-0410-8362-fc8e0e1515e5

--- tdl/Plotter.py
+++ tdl/Plotter.py
@@ -129,7 +129,7 @@ def _getPlot(tdl=None):
 
 def tdl_plot(x,y=None,tdl=None,**kw):
     p = _getPlot(tdl=tdl)
-    if p is not None:  p.plot(x,y=y,**kw)
+    if p is not None:  p.oplot(x,y=y,**kw)
     else:       print 'cannot plot?'
 
 def tdl_newplot(x,y=None,tdl=None,**kw):
