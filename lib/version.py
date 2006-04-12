5fd4f701c5200c3c6e4295a0555c1e467379eefb
newville <newville@cc796bef-3d10-0410-8362-fc8e0e1515e5>
2006-04-12 21:46:33 +0000

    0.3.3
    git-svn-id: http://millenia.cars.aps.anl.gov/svn/tdl/trunk@58 cc796bef-3d10-0410-8362-fc8e0e1515e5

--- lib/version.py
+++ lib/version.py
@@ -1,6 +1,6 @@
 # tdl version information
-version = '0.3.2'
-date    = '2006-March-25'
+version = '0.3.3'
+date    = '2006-April-12'
 year    = '2006'
 author  = 'Matthew Newville, Tom Trainor'
 email   = 'newville@cars.uchicago.edu'
