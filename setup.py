4262d4758e40d60a9cbb98ba7d9adb55aea8742e
newville <newville@cc796bef-3d10-0410-8362-fc8e0e1515e5>
2006-04-18 17:16:02 +0000

    mostly working setup.py , modules installed to under site-packages/tdl
    git-svn-id: http://millenia.cars.aps.anl.gov/svn/tdl/trunk@84 cc796bef-3d10-0410-8362-fc8e0e1515e5

--- setup.py
+++ setup.py
@@ -5,14 +5,20 @@ import distutils
 from distutils.core import setup, Extension
 
 from  lib.version import name,version,author,email,desc
+
+import os, glob
+package_dir = {'tdl': 'lib', 'tdl.modules': 'modules'}
+packages = ['tdl','tdl.TkPlotter','tdl.FileIO','tdl.modules','tdl.modules.GUI','tdl.modules.IO']
+
+
 setup(
     name = name,
     version = version,
     author =  author,
     author_email = email,
     description = desc,
-    package_dir = {'tdl': 'lib'},
-    packages = ['tdl','tdl.TkPlotter'],
+    package_dir = package_dir,
+    packages = packages,
     license = 'Python',
     data_files  = [('bin',['tdl'])]
 )
