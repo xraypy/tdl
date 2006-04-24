263ac31dd4812dd7c342b7f7103ef003741fa141
tdl <tdl@cc796bef-3d10-0410-8362-fc8e0e1515e5>
2006-04-24 02:35:21 +0000

    add preference for scipy
    git-svn-id: http://millenia.cars.aps.anl.gov/svn/tdl/trunk@93 cc796bef-3d10-0410-8362-fc8e0e1515e5

--- lib/Num.py
+++ lib/Num.py
@@ -4,19 +4,35 @@
 # --------------
 # Modifications
 # --------------
+#  23-Apr-2006  MN  added test for scipy 0.4.8 or higher, required for
+#                   some numeric functionality
+#
 #  21-Mar-2006  MN  switched to requiring NumPy 0.9.6 or higher
 #
 ##########################################################################
 from __future__ import division
 
-version_needed = 'Need NumPy version 0.9.6 or higher'
+numpy_needed = 'Need SciPy 0.4.8 or (at least) NumPy version 0.9.6 or higher'
+num_version = None
 try:
-    import numpy as Num
+    import scipy as Num
     v = [int(i) for i in Num.__version__.split('.')]
-    if v[1]<9 or v[2]<6: raise ImportError, version_needed
+    v = 100*v[0] + 10*v[1] + v[2]
+    if v >= 47:
+        num_version = 'scipy %s' % Num.__version__
 except:
-    raise ImportError, version_needed
-    
+    try:
+        import numpy as Num
+        v = [int(i) for i in Num.__version__.split('.')]
+        v = 100*v[0] + 10*v[1] + v[2]
+        if v >= 96:
+            num_version = 'numpy %s' % Num.__version__            
+    except:
+        pass
+
+if num_version is None:
+    raise ImportError, numpy_needed
 
 if __name__ == '__main__':
-    print 'using numpy version ', Num.__version__
+    print 'tdl will use %s ' % num_version
+    
