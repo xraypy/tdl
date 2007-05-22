18fce8f5385abb3f75551b00498c5b68e24f2bae
newville <newville@cc796bef-3d10-0410-8362-fc8e0e1515e5>
2007-05-22 20:13:06 +0000

    now using scipy.signal for fast cubic splines
    git-svn-id: http://millenia.cars.aps.anl.gov/svn/tdl/trunk@234 cc796bef-3d10-0410-8362-fc8e0e1515e5

--- modules/xray/CarsMca/spline.py
+++ modules/xray/CarsMca/spline.py
@@ -18,6 +18,5 @@ def spline_interpolate(oldx, oldy, newx, smoothing=0.001, **kw):
       newy = spline_interpolate(oldx, oldy, newx)
 
       1-dimensional cubic spline, for cases where oldx and newx are on a uniform grid.
-
     """
     return cspline1d_eval(cspline1d(oldy), newx, dx=oldx[1]-oldx[0],x0=oldx[0])
