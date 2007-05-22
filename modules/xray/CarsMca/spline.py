b759e1e120808f6c5c67ec1811c1cb1c26e4ab6d
newville <newville@cc796bef-3d10-0410-8362-fc8e0e1515e5>
2007-05-22 19:15:04 +0000

    now using scipy.signal for fast cubic splines
    git-svn-id: http://millenia.cars.aps.anl.gov/svn/tdl/trunk@233 cc796bef-3d10-0410-8362-fc8e0e1515e5

--- modules/xray/CarsMca/spline.py
+++ modules/xray/CarsMca/spline.py
@@ -1,9 +1,23 @@
-""" cubic spline:  now using scipy.interpolate splrep and splev """
+""" cubic spline:  now using scipy.signal and/or scipy.interpolate """
 
 from scipy.interpolate import splrep, splev
-def spline_interpolate(oldx, oldy, newx, smoothing=0.001, **kw):
-    """ newy = spline_interpolate(oldx, oldy, newx)
+from scipy.signal import cspline1d, cspline1d_eval
+
+def spline_interpolate_general(oldx, oldy, newx, smoothing=0.001, **kw):
+    """ newy = spline_interpolate_general(oldx, oldy, newx)
+
+     handles multi-dimensional data, non-uniform x-grids, but is
+     much slower than spline_interpolate for 1d cubic splines
 
     """
     rep = splrep(oldx,oldy,s=smoothness,full_output=False,**kw)
     return splev(newx, rep)
+
+def spline_interpolate(oldx, oldy, newx, smoothing=0.001, **kw):
+    """
+      newy = spline_interpolate(oldx, oldy, newx)
+
+      1-dimensional cubic spline, for cases where oldx and newx are on a uniform grid.
+
+    """
+    return cspline1d_eval(cspline1d(oldy), newx, dx=oldx[1]-oldx[0],x0=oldx[0])
