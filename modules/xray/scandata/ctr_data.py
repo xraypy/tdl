00103c76b2fac793c2dff5c373baae1997023734
(no author) <(no author)@cc796bef-3d10-0410-8362-fc8e0e1515e5>
2009-07-16 19:15:29 +0000

    scale correction by 17000
    git-svn-id: http://millenia.cars.aps.anl.gov/svn/tdl/trunk@331 cc796bef-3d10-0410-8362-fc8e0e1515e5

--- modules/xray/scandata/ctr_data.py
+++ modules/xray/scandata/ctr_data.py
@@ -66,8 +66,8 @@ class CtrData:
             self.I0   = Num.append(self.I0,scan.scalers['io'])
             self.I    = Num.append(self.I,scan.image_peaks[self.I_lbl])
             self.Ierr = Num.append(self.Ierr,scan.image_peaks[self.Ierr_lbl])
-            # the correction Factor is multiplied by 1000 to rescale to Counts
-            corr = self.calc_correction(scan)*1000 
+            # the correction Factor is multiplied by 17000 to rescale to Counts/sec
+            corr = self.calc_correction(scan)*17000 
             F    = (corr*scan.image_peaks[self.I_lbl])**0.5
             Ferr = (corr*scan.image_peaks[self.Ierr_lbl])**0.5
             #
