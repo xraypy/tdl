86d8a369637b142d50d17d3eac3996ca9834b841
(no author) <(no author)@cc796bef-3d10-0410-8362-fc8e0e1515e5>
2009-12-13 04:37:52 +0000

    add changes
    git-svn-id: http://millenia.cars.aps.anl.gov/svn/tdl/trunk@408 cc796bef-3d10-0410-8362-fc8e0e1515e5

--- modules/xtal/xtal_calc.py
+++ modules/xtal/xtal_calc.py
@@ -12,11 +12,6 @@ Modifications:
 ##########################################################################
 """
 Todo
- - Lattice:
-    - add cell volume calcs
-    - add real_to_recip and recip_to_real (ie get vector indicies
-      in different basis)
-    - calculate angles between real and recip vectors "angle_rr"
 
  - LatticeTransform class 
 
@@ -30,6 +25,133 @@ Todo
  
 """
 ##########################################################################
+"""
+Notes on Convention and Generalized Transforms
+----------------------------------------------
+The set of basis vectors that defines the real lattice are
+covarient quantities, while the set of indicies that define
+real space vectors are contravarient:
+                 |a|
+    v = (x,y,z)* |b| = x*a + y*b + z*c
+                 |c|
+If A is an arbitrary matrix that transforms the contravarient
+vector indicies, its assumed that the indicies left multiply 
+    (x',y',z') = (x,y,z) * A
+
+Note that the usual convention is to express covarient quantities
+as column vectors and contravarient quantities as row vectors
+(subscripts and superscripts respectivley in tensor notation)
+However, in the notes below we'll just take 1-D arrays to be
+column vectors to simplify the notation.  Therefore, if A is defined
+as above, the matrix vector multiplications will need to be changed to:
+    |x'|                 |x|
+    |y'| = transpose(A)* |y|
+    |z'|                 |z|
+We can express the same in numpy given a vector v and matrix A 
+    dot(v,A) = dot(transpose(A),v)
+where v is multiplied as a row vector on the lhs and a column
+vector on the rhs; ie the transpose of v bewteen column and
+row vecor is implied by its position.
+
+Assume there is a matrix that transforms the covarient basis
+(via rotation and/or dilation) to a new set of basis vectors:
+    |a'|      |a|        a' = (F11*a + F12*b + F13*c)
+    |b'| = F* |b|   or   b' = (F21*a + F22*b + F23*c)
+    |c'|      |c|        c' = (F31*a + F32*b + F33*c)
+with a well defined inverse:
+    |a|           |a'|   
+    |b| = inv(F)* |b'|   
+    |c|           |c'|   
+
+The covarient/contravarient relationship bewteen basis vectors
+and the vector indicies implies that the indicies of a (stationary)
+vector in the new (') basis are obtained via the inverse of the
+F matrix:
+    |x'|      |x|
+    |y'| = M* |y|
+    |z'|      |z|
+where
+    M = transpose(inv(F)) = inv(transpose(F))
+
+The reciprocal relation is:
+    |x|      |x'|
+    |y| = N* |y'|
+    |z|      |z'|
+where
+    N = inv(M) = transpose(F)
+
+With respect to the real lattice the recip lattice basis vectors
+are contravarient and the vector indicies are covarient:
+                     |h|
+    vr = (ar,br,cr)* |k| = h*ar + k*br + l*cr
+                     |l|
+
+Given the generalized basis transform F above, the covarient recip
+lattice indicies transform in the same way as the lattice.
+Therefore:
+     |h'|      |h|
+     |k'| = F* |k|
+     |l'|      |l|
+     
+The reciprocal relation is:
+    |h|      |k'|
+    |k| = G* |k'|
+    |l|      |l'|
+where
+    G = inv(F) = transpose(M)
+
+
+The metric tensor
+------------------
+The metric tensor is defined in terms of the vector dot product
+for a general real space system
+  u*v = u*g*v
+where
+       |a*a, a*b, a*c|
+  g =  |b*a, b*b, b*c|
+       |c*a, c*b, c*c|
+The metric tensor may also be defined in terms of a lattice transform
+ie the real space basis vectors can be expressed in terms of the
+recip space basis vectors via
+      |a|      |ar|
+      |b| = g* |br|
+      |c|      |cr|
+therefore
+      |ar|       |a|
+      |br| = gr* |b|
+      |cr|       |c|
+where gr = inv(g).  Hence the recip lattice vectors
+can be plotted in the real lattice via
+      ar = gr11*a + gr12*b + gr13*c
+      br = gr21*a + gr22*b + gr23*c
+      cr = gr31*a + gr32*b + gr33*c
+and visa versa
+
+Using the above relations for lattice transforms the
+indicies of real lattice vectors can be calc in the recip
+lattice and visa-versa via:
+   |h|                             |x|      |x|
+   |k| = (x,y,z)*g = transpose(g)* |y| = M* |y|
+   |l|                             |z|      |z|
+and
+   |x|                      |h|      |h|
+   |y| = inv(transpose(g))* |k| = N* |k|
+   |z|                      |l|      |l|
+
+e.g. in above we take recip lattice as primed basis and
+real lattice as the unprimed basis, therefore equate:
+  F = gr
+  G = inv(F) = inv(gr) = g 
+  M = transpose(inv(F)) = transpose(g)
+  N = inv(M) = transpose(F) = inv(transpose(g)) = transpose(gr)
+
+Note that g and gr are always symmetric, therefore
+  g = transpose(g) and gr = transpose(gr).
+  F = N = gr
+  G = M = g
+  
+"""
+##########################################################################
 
 import numpy as num
 import sys
@@ -40,6 +162,7 @@ class Lattice:
     """
     Class that defines a lattice and various
     operations within the lattice
+
     """
     def __init__(self,a=1.,b=1.,c=1.,alpha=90.,beta=90.,gamma=90.):
         self.update(a=a,b=b,c=c,alpha=alpha,beta=beta,gamma=gamma)
@@ -59,7 +182,7 @@ class Lattice:
 
     def update(self,a=None,b=None,c=None,alpha=None,beta=None,gamma=None):
         """
-        update lattice parameters
+        Update lattice parameters
         """
         if a != None: self.a = float(a)
         if b != None: self.b = float(b)
@@ -72,7 +195,7 @@ class Lattice:
 
     def _calc_g(self):
         """
-        calculate the metric tensors and recip lattice params
+        Calculate the metric tensors and recip lattice params
            self.g  = real space metric tensor
            self.gr = recip space metric tensor
         """
@@ -98,23 +221,27 @@ class Lattice:
         
     def cell(self):
         """
-        return array of real lattice cell parameters
+        Return array of real lattice cell parameters
         """
         return num.array([self.a,self.b,self.c,
                           self.alpha,self.beta,self.gamma],dtype=float)
 
     def rcell(self):
         """
-        return array of reciprocal lattice cell parameters
+        Return array of reciprocal lattice cell parameters
         """
         return num.array([self.ar,self.br,self.cr,
                           self.alphar,self.betar,self.gammar],dtype=float)
         
-    def V(self,recip=False):
+    def vol(self,recip=False):
         """
         Calculate the cell volume.
           If recip == False, this is the real space vol in ang**3
           If recip == True, this is the recip space vol in ang**-3
+
+        V_real  = sqrt(determiant(g))
+        V_recip = sqrt(determiant(inv(g)))
+
         """
         if recip == True: g = self.gr
         else: g = self.g
@@ -126,24 +253,29 @@ class Lattice:
         
     def dot(self,u,v,recip=False):
         """
-        calculate dot product of two vectors
-        if u and v are real space vectors, recip = False
-        if u and v are recip space vectors, recip = True
+        Calculate dot product of two vectors
+          If u and v are real space vectors, use recip = False
+          If u and v are recip space vectors, use recip = True
         Note u and v are assumed to be normal numpy arrays
         (ie not matrix objects)
+
+        The generalized dot product for real space vectors
+        may be defined in terms of the metric tensor (g):
+           u*v = u*g*v
+        If u and v are recip lattice vectors replace g with
+        gr = inv(g)
         """
         if recip == True: g = self.gr
         else: g = self.g
         u = num.array(u,dtype=float)
         v = num.array(v,dtype=float)
-        # dot product = u*g*v, from Sands.
         return num.dot(u,num.dot(g,v))
 
     def mag(self,v,recip=False):
         """
-        calculate the norm of a vector
-        if v is real space vector, recip = False
-        if v is recip space vector, recip = True
+        Calculate the norm of a vector
+          If v is real space vector, use recip = False
+          If v is recip space vector, use recip = True
         Note v is assumed to be normal numpy array
         (ie not matrix objects)
         """
@@ -152,9 +284,9 @@ class Lattice:
 
     def angle(self,u,v,recip=False):
         """
-        calculate dot product of two vectors
-        if u and v are real space vectors, recip = False
-        if u and v are recip space vectors, recip = True
+        Calculate dot product of two vectors
+          If u and v are real space vectors, use recip = False
+          If u and v are recip space vectors, use recip = True
         Note u and v are assumed to be normal numpy arrays
         (ie not matrix objects)
         """
@@ -167,24 +299,40 @@ class Lattice:
         alpha = num.arccos(arg)
         return num.degrees(alpha)
 
+    def angle_rr(self,x,h):
+        """
+        Calculate the angle between a real vector x = [x,y,z]
+        and a recip vector h = [h,k,l]
+        """
+        x = num.array(x,dtype=float)
+        h = num.array(h,dtype=float)
+        hx = x*h
+        xm = self.mag(x,recip=False)
+        hm = self.mag(h,recip=True)
+        arg = hx/(hm*xm)
+        if num.fabs(arg) > 1.0:
+            arg = arg / num.fabs(arg)
+        alpha = num.arccos(arg)
+        return num.degrees(alpha)
+
     def d(self,hkl):
         """
-        calculate d space for given [h,k,l]
+        Calculate d space for given [h,k,l]
         """
         if len(hkl)!=3:
             print "Error need an array of [h,k,l]"
             return 0.
-        H = self.mag(hkl,recip=True)
-        if H == 0.:
+        h = self.mag(hkl,recip=True)
+        if h == 0.:
             print "Error [h,k,l] magnitude is zero:", hkl
             return 0.0
-        d = 1./H
+        d = 1./h
         return d
 
     def tth(self,hkl,lam=1.5406):
         """
-        calculate 2Theta for given [h,k,l] and lambda
-        default lambda = Cu Ka1
+        Calculate 2Theta for given [h,k,l] and lambda
+        Default lambda = Cu Ka1
         """
         d = self.d(hkl)
         if d == 0.: return 0.
@@ -197,19 +345,37 @@ class Lattice:
 
     def dvec(self,hkl):
         """
-        calculate the real space vector d
+        Calculate the real space vector d
         which has a magnitude of the d spacing
-        and is normal to the plane HKL
+        and is normal to the plane hkl = [h,k,l]
         """
+        # convert hkl vector to real space indicies
+        #hkl  = num.array(hkl,dtype=float)
+        #dvec = num.dot(self.gr,hkl)
+        dvec = self.recip_to_real(hkl)
         dspc = self.d(hkl)
         if dspc == 0: return num.array([0.,0.,0.])
-        # convert hkl vector to real space indicies
-        hkl  = num.array(hkl,dtype=float)
-        dvec = num.dot(self.gr,hkl)
         dvec = (dspc**2.)*dvec
         return dvec
 
-
+    def recip_to_real(self,hkl):
+        """
+        Given recip vector hkl = [h,k,l] calculate the
+        vectors indicies in the real lattice
+        """
+        hkl = num.array(hkl,dtype=float)
+        #v   = num.dot(self.gr.transpose(),hkl)
+        v   = num.dot(self.gr,hkl)
+        return v
+    
+    def real_to_recip(self,v):
+        """
+        Given real vector v = [x,y,z] calculate the
+        vectors indicies in the reciprocal lattice
+        """
+        v   = num.array(v,dtype=float)
+        hkl = num.dot(v,self.g)
+        return hkl
 
 ##########################################################################
 """
